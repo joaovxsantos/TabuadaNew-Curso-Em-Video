0d69a3a4dd8e832cb87ed2ff3b8c228f14065ac9
João Vitor <joaovitor55670@gmail.com>
2022-09-08 15:59:08 -0300

    Final-Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let submit = document.querySelector('#submit')
+let valor = document.querySelector('#valor')
+
+
+function account() {
+
+    let valor = document.querySelector('#valor').value
+    let resultado = document.querySelector('#p2')
+
+    if (!valor) {
+        alert('Digite um número!')
+    } else {
+        resultado.innerText = ""
+        for (i = 0; i <= 10; i++) {
+            resultado.innerText += `${valor} x ${i} = ` + valor * i + "\n"
+        }
+    }
+}
+
+valor.addEventListener('keypress', function (e) {
+    if (e.keyCode == 13) {
+        account()
+    }
+})
+
+submit.addEventListener('click', account)
